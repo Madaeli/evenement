bc3a8c34e93f7ff81fb65bd08d14404e33eee096
Madaeli <myvaneli@gmail.com>
2023-06-02 22:09:30 +0200

    View

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
       return(
