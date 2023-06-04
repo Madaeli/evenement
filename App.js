8a11e068622e23c64a650ff70d15845dcb2884fa
Madaeli <myvaneli@gmail.com>
2023-06-04 05:39:56 +0200

    Head

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
-import { Text, View } from "react-native"
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { s } from "./app.sytle";
+import { Text, View} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from  "react-native-safe-area-context";
+import { s } from "./app.style";
+import { Header } from "./components/Header/Header.jsx";
+import { CardTodo } from "./components/CardTodo/CardTodo.jsx"
+
+const TODO_LIST = [
+  {id: 1, title : "Sortir le chien", isCompleted: true },
+  {id: 1, title : "Aller chez le garagiste", isCompleted: false },
+  {id: 1, title : "Faire les courses", isCompleted: true },
+  {id: 1, title : "Appeler le vétérinaire", isCompleted: true },
+];
 export default function App() {
       return(
+          <>
             <SafeAreaProvider>
                   <SafeAreaView style = { s.app }>
                         <View style={s.header}>
-                              <Text>HeaderTest</Text>
+                          <Header/> 
                         </View>
                         <View style={s.body}>
-                              <Text>Body</Text>
-                        </View>
-                              
-                        <View style={s.footer}>
-                              <Text>Footer</Text>
-                        </View>
-                            
+                             <CardTodo todo={TODO_LIST[0]} />
+                        </View>                 
                   </SafeAreaView>  
             </SafeAreaProvider>
-                 
+            <View style={s.footer}>
+                              <Text>Footer</Text>
+          </View>
+          </>       
       );
 }
 
--- app.style.js
+++ app.style.js
@@ -4,9 +4,11 @@ import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
     app: {
         backgroundColor: "#F9F9F9" ,
+        
         flex: 1,
+        padding: 10,
     },
-    header: {},
-    body: {},
-    footer: {},
+    header: {flex: 1,},
+    body: {flex: 5,},
+    footer: {height: 70},
 })
